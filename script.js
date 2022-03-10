b24e8a307619d035f7731a24b57942e662f268aa
Abhijeet Mishra <colouredpages@gmail.com>
2022-03-10 21:42:23 +0530

    taskyDoneOneLevel

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let i=0;
-    let colorArray = ['#DC136C','#353A47',"#885A5A",'#C45AB3','#631A86'];
-Array.from(document.getElementsByClassName("date")).forEach(item => {item.innerHTML = new Date().toDateString();
+let i = 0;
+let colorArray = ["#DC136C", "#353A47", "#885A5A", "#C45AB3", "#631A86"];
+const dateElements = document.getElementsByClassName("date");
+Array.from(dateElements).forEach((item) => {
+  item.innerHTML = new Date().toDateString();
 });
-Array.from(document.getElementsByClassName("category")).forEach(item => item.style.backgroundColor = colorArray[i++%colorArray.length])
+Array.from(document.getElementsByClassName("category")).forEach(
+  (item) => (item.style.backgroundColor = colorArray[i++ % colorArray.length])
+);
+
+
