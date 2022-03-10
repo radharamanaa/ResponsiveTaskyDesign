a4eeee7bee0822ab643aa918c2122b5a8287bc63
Abhijeet Mishra <colouredpages@gmail.com>
2022-03-10 20:20:20 +0530

    Nice flex cards - no layout yet

---
+++ script.js
@@ -0,0 +1,5 @@
+let i=0;
+    let colorArray = ['#DC136C','#353A47',"#885A5A",'#C45AB3','#631A86'];
+Array.from(document.getElementsByClassName("date")).forEach(item => {item.innerHTML = new Date().toDateString();
+});
+Array.from(document.getElementsByClassName("category")).forEach(item => item.style.backgroundColor = colorArray[i++%colorArray.length])
